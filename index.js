7a0898d96b718fddc2c0c6d4ee34c07acb204aae
Lucas Piputto <lucaspiputto@gmail.com>
2021-08-06 01:02:10 -0300

    Connect to MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
 const express = require('express');
 
 const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+const db = 'mongodb://localhost/mecu-db';
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    console.log('Connected succesfully to', db);
+  })
+  .catch(err => {
+    console.log('Connection error:', err);
+  });
+
+app.use('/api', require('./api/routes/products'));
 
 app.get('/', (req,res) => {
   res.send('<h1>Hola</h1>');
