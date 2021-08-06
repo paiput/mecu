5f7305141d7839723de435a1a9aa3cbdc4d089c4
Lucas Piputto <lucaspiputto@gmail.com>
2021-08-06 01:09:49 -0300

    Add React Router and useEffect to fetch products

--- client/src/App.js
+++ client/src/App.js
@@ -4,52 +4,30 @@ import {
   Switch,
   Route
 } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
+import productService from './services/products';
 // components
 import { Header } from './components/Header';
 import { FeaturedProduct } from './components/products/FeaturedProduct';
 import { LatestProducts } from './components/products/LatestProducts';
-
-const products = [
-  {
-    id: 1,
-    name: 'Remera',
-    description: 'Remera para entrenamiento de tela ultrafina',
-    img: null,
-    price: 2499,
-    quantity: 10
-  },
-  {
-    id: 2,
-    name: 'Pantalon',
-    description: 'Pantalon de jean',
-    img: null,
-    price: 1999,
-    quantity: 5
-  },
-  {
-    id: 3,
-    name: 'Gorra',
-    description: 'Gorra de beisbol',
-    img: null,
-    price: 800,
-    quantity: 11
-  },
-  {
-    id: 4,
-    name: 'Camiseta NBA',
-    description: 'Camiseta NBA de los Chicago Bulls',
-    img: null,
-    price: 9000,
-    quantity: 3
-  }
-]
-
-const randomProduct = products[Math.floor(Math.random() * products.length)];
+import { ProductDetails } from './components/products/ProductDetails';
+import { SellForm } from './components/user/SellForm';
 
 const App = () => {
-  const [user, setUser] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const randomProduct = products[Math.floor(Math.random() * products.length)];
+
+  useEffect(() => {
+    productService.getAll()
+      .then(fetchedProducts => {
+        console.log('Products fetched');
+        setProducts(fetchedProducts);
+        setLoading(false);
+      })
+  }, []);
 
   return (
     <Router>
@@ -57,8 +35,23 @@ const App = () => {
         <Header />
         <Switch>
           <Route exact path="/">
-            <FeaturedProduct product={randomProduct} />
-            <LatestProducts products={products} />
+            {
+              loading
+                ? 'Cargando...'
+                : <>
+                  <FeaturedProduct product={randomProduct} />
+                  <LatestProducts products={products} />
+                </>
+            }
+          </Route>
+          <Route path="/products/:id">
+            <ProductDetails />
+          </Route>
+          <Route path="/sell">
+            <SellForm />
+          </Route>
+          <Route path="/">
+            <h2>Not found</h2>
           </Route>
         </Switch>
       </div>
