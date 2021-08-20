99b03a5d4650ef035434a997ece953d7e9755e11
Lucas Piputto <lucaspiputto@gmail.com>
2021-08-20 00:23:50 -0300

    Add relationship with User model

--- api/models/Product.js
+++ api/models/Product.js
@@ -6,7 +6,16 @@ const ProductSchema = new Schema({
   // img: String,
   price: { type: Number, required: true },
   quantity: { type: Number, required: true },
-  createdAt: { type: Date, default: Date.now() }
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
+});
+
+ProductSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    delete returnedObject.__v;
+  }
 });
 
 const Product = model('Product', ProductSchema);
