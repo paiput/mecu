95af8189ab7e7969562db326e9eecf2587b3798d
Lucas Piputto <lucaspiputto@gmail.com>
2021-08-22 01:14:54 -0300

    Set passport strategy configuration

---
+++ api/passportConfig.js
@@ -0,0 +1,31 @@
+const User = require('./models/User');
+const bcrypt = require('bcrypt');
+const LocalStrategy = require('passport-local').Strategy;
+
+module.exports = (passport) => {
+  passport.use(new LocalStrategy(
+    (username, password, done) => {
+      User.findOne({ username: username }, (err, user) => {
+        if (err) return done(err);
+        if (!user) {
+          return done(null, false, { message: 'Nombre de usuario incorrecto.' });
+        }
+        bcrypt.compare(password, user.passwordHash, (err, result) => {
+          if (err) return done(err);
+          if (result === true ) return done(null, user);
+          else return done(null, false, { message: 'Contraseña incorrecta.' });
+        })
+      });
+    }
+  ));
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+      done(err, user);
+    });
+  });
+}
