87c31171ff31de67d5464d0c1ce4dbc078a8d2ff
Lucas Piputto <lucaspiputto@gmail.com>
2021-08-31 21:38:12 -0300

    Refactor handlers and add CartContext

--- client/src/App.js
+++ client/src/App.js
@@ -9,22 +9,28 @@ import './App.css';
 import productService from './services/products';
 import handleService from './services/handlers';
 import loginService from './services/login';
-import { UserContext } from './contexts/UserContext'; // estado global del usuario
+import { UserContext } from './contexts/UserContext'; // contexto del usuario
+import { CartContext } from './contexts/CartContext'; // contexto del carrito del usuario
 // components
-import { Header } from './components/Header';
+import { Header } from './components/header/Header';
 import { FeaturedProduct } from './components/products/FeaturedProduct';
 import { LatestProducts } from './components/products/LatestProducts';
 import { ProductDetails } from './components/products/ProductDetails';
 import { SellForm } from './components/user/SellForm';
 import { RegisterForm } from './components/user/RegisterForm';
 import { LoginForm } from './components/user/LoginForm';
+import { Account } from './components/user/Account';
 
 const App = () => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null); // estado global del usuario
+  const [cart, setCart] = useState([]); // estado global del carrito del usuario
   const [products, setProducts] = useState([]);
   const [randomProduct, setRandomProduct] = useState({});
   const [loading, setLoading] = useState(true);
+
+  // estados para detectar clicks fuera de los menúes desplegables
   const [hambMenu, setHambMenu] = useState(false);
+  const [cartContainer, setCartContainer] = useState(false);
 
   useEffect(() => {
     productService.getAll()
@@ -48,41 +54,51 @@ const App = () => {
     console.log('usuario:', user);
   }
 
+  const handleClickOutsideMenus = (e) => {
+    handleService.handleClickOutsideHambMenu(e, setHambMenu)
+    handleService.handleClickOutsideCartContainer(e, setCartContainer)
+  }
+
   return (
     <Router>
-      <div className="App" onClick={(e) => handleService.handleClickOutsideHambMenu(e, setHambMenu)}>
+      <div className="App" onClick={(e) => handleClickOutsideMenus(e)}>
         <UserContext.Provider value={{ user, setUser }} >
-          <button onClick={getUser}>mostar usuario</button>
-          <Header hambMenu={hambMenu} setHambMenu={setHambMenu} />
-          <Switch>
-            <Route exact path="/">
-              {
-                loading
-                  ? 'Cargando...'
-                  : products.length === 0
+          <CartContext.Provider value={{ cart, setCart }} >
+            <button onClick={getUser}>mostar usuario</button>
+            <Header hambMenuState={{hambMenu, setHambMenu}} cartState={{cartContainer, setCartContainer}} />
+            <Switch>
+              <Route exact path="/">
+                {
+                  loading
+                    ? 'Cargando...'
+                    : products.length === 0
                     ? <p>Aún no hay productos publicados, sé el primero.</p>
                     : <>
-                        <FeaturedProduct product={randomProduct} />
-                        <LatestProducts products={products} />
-                    </>
-              }
-            </Route>
-            <Route path="/products/:id">
-              <ProductDetails />
-            </Route>
-            <Route path="/sell">
-              <SellForm />
-            </Route>
-            <Route path="/register">
-              <RegisterForm />
-            </Route>
-            <Route path="/login">
-              <LoginForm />
-            </Route>
-            <Route path="/">
-              <h2>Not found</h2>
-            </Route>
-          </Switch>
+                          <FeaturedProduct product={randomProduct} />
+                          <LatestProducts products={products} />
+                      </>
+                }
+              </Route>
+              <Route path="/products/:id">
+                <ProductDetails />
+              </Route>
+              <Route path="/sell">
+                <SellForm />
+              </Route>
+              <Route path="/register">
+                <RegisterForm />
+              </Route>
+              <Route path="/login">
+                <LoginForm />
+              </Route>
+              <Route path="/account">
+                <Account />
+              </Route>
+              <Route path="/">
+                <h2>Not found</h2>
+              </Route>
+            </Switch>
+          </CartContext.Provider>
         </UserContext.Provider>
       </div>
     </Router>
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,11 +1,26 @@
 // imports
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import handleService from '../../services/handlers';
 import * as Icon from 'react-bootstrap-icons';
 // components
 import { HambMenu } from './HambMenu';
+import { CartContainer } from './CartContainer';
+// CartContext
+import { CartContext } from '../../contexts/CartContext';
 
-export const Header = ({ hambMenu, setHambMenu }) => {
+export const Header = ({ hambMenuState, cartState }) => {
+  const { hambMenu, setHambMenu } = hambMenuState;
+  const { cartContainer, setCartContainer } = cartState;
+  const { cart } = useContext(CartContext);
+
+  const handleShowHambMenu = () => {
+    handleService.handleClick(setHambMenu);
+  }
+
+  const handleShowCart = () => {
+    handleService.handleClick(setCartContainer);
+  }
 
   return (
     <>
@@ -21,17 +36,19 @@ export const Header = ({ hambMenu, setHambMenu }) => {
             <input type="text" placeholder="Buscar productos..."></input>
           </form>
           <div className="tools-container">
-            <div className={`nav__hamburger ${hambMenu ? 'change-to-cross' : ''}`} onClick={() => handleService.handleHambMenuClick(setHambMenu)} >
+            <div className={`nav__hamburger ${hambMenu ? 'change-to-cross' : ''}`} onClick={(handleShowHambMenu)} >
               <div className="nav__hamburger-child"></div>
               <div className="nav__hamburger-child"></div>
               <div className="nav__hamburger-child"></div>
             </div>
-            <button className="cart-button">
+            <button className="cart-button" onClick={handleShowCart}>
               <Icon.Cart className="icon big-icon" />
+              <span className="cart-button__products-counter">{cart.length > 9 ? '9+' : cart.length}</span>
             </button>
           </div>
         </nav>
       </header>
+      { cartContainer && <CartContainer />}
       { hambMenu && <HambMenu /> }
     </>
   )
