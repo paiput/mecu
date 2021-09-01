b54dc5de7a10bdca4f0b0a9330b6cace0c06f57a
Lucas Piputto <lucaspiputto@gmail.com>
2021-09-01 18:28:15 -0300

    Add styles to Account

--- client/src/components/user/Account.jsx
+++ client/src/components/user/Account.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import handleService from '../../services/handlers';
 // UserContext
 import { useContext } from 'react';
 import { UserContext } from '../../contexts/UserContext';
@@ -7,17 +8,22 @@ import userImg from './user.png';
 
 export const Account = () => {
   const { user } = useContext(UserContext);
-
+  console.log('este sos vos', user)
   return (
-    <div>
-      <div className="user-img__container">
-        <img src={userImg} alt="perfil de usuario" />
+    <div className="user-container">
+      <div className="user__profile">
+        <div className="user__img-container">
+          <img src={userImg} alt="perfil de usuario" className="user__img" />
+        </div>
+        <h2>{user.username}</h2>
+        <h3>{user.name} {user.surname}</h3>
       </div>
-      <h2>{user.username}</h2>
-      <h3>{user.name} {user.surname}</h3>
-      <Link to="/account">Mis publicaciones</Link>
-      {/* <Link>Balance</Link> */}
-      <button>Cerrar sesión</button>
+      <h3>Billetera: ${handleService.numberWithCommas(user.balance)}</h3>
+      <Link to="/account/balance" className="user__link">Cargar dinero</Link>
+      <Link to="/account/publishedproducts" className="user__link">Mis publicaciones</Link>
+      <Link to="/account/purchasedproducts" className="user__link">Historial de compra</Link>
+      <Link to="/account/productssold" className="user__link">Historial de venta</Link>
+      <button style={{display: 'block', margin: '2rem 0', padding: '.5rem 1rem'}}>Cerrar sesión</button>
     </div>
   )
 }
