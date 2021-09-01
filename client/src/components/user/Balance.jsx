5c33d4365b4d01b4d47954c3617dbc83192001fb
Lucas Piputto <lucaspiputto@gmail.com>
2021-09-01 18:19:33 -0300

    Create Balance component

---
+++ client/src/components/user/Balance.jsx
@@ -0,0 +1,59 @@
+import { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import { UserContext } from '../../contexts/UserContext';
+import usersService from '../../services/users';
+import handleService from '../../services/handlers';
+// components
+import { InputMsg } from './InputMsg';
+
+export const Balance = () => {
+  const { user, setUser } = useContext(UserContext);
+  const { register, handleSubmit, formState: { errors }, reset } = useForm();
+
+  const filterNotNumbers = (e) => {
+    if (e.which < 48 || e.which > 57) e.preventDefault();
+  }
+
+  const onSubmit = data => {
+    const amountToLoad = data;
+
+    console.log('Cantidad a cargar:', amountToLoad);
+
+    usersService.updateUserBalance(user.username, amountToLoad)
+      .then(updatedBalance => {
+        console.log('Saldo aumentado exitosamente.\nSaldo actual:', updatedBalance);
+        setUser(userData => {
+          return {...userData, balance: updatedBalance }
+        })
+      })
+      .catch(err => {
+        alert('Lo sentimos, no se pudo cargar el dinero');
+        console.log('Error on POST:', err);
+      });
+
+    reset();
+  }
+
+  return (
+    <div>
+      <h2>Tu saldo actual</h2>
+      <h3>${handleService.numberWithCommas(user.balance)}</h3>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="amountToLoad">$</label>
+        <input 
+          type="number"
+          id="input__amount-to-load"
+          onKeyPress={filterNotNumbers}
+          {...register("amountToLoad", { 
+            required: true,
+            min: 1, 
+            max: 10000
+          })}
+        />
+        <button type="submit">Cargar saldo</button>
+        {errors.amountToLoad?.type === 'max' && <InputMsg msg="La cantidad máxima de saldo que se puede cargar es de $10,000" />}
+        {errors.amountToLoad?.type === 'min' && <InputMsg msg="No se puede cargar menos de $1" />}
+      </form>
+    </div>
+  )
+}
