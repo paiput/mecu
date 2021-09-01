e8a5512c929ce5b34f0aa0023319eca2909a5ef7
Lucas Piputto <lucaspiputto@gmail.com>
2021-09-01 10:07:45 -0300

    Check if user is logged to show cart products

--- client/src/components/header/CartContainer.jsx
+++ client/src/components/header/CartContainer.jsx
@@ -1,9 +1,11 @@
 import { useContext } from 'react';
 import { CartContext } from '../../contexts/CartContext';
+import { UserContext } from '../../contexts/UserContext';
 // components
 import { CartProduct } from './CartProduct';
 
 export const CartContainer = () => {
+  const { user } = useContext(UserContext);
   const { cart, setCart } = useContext(CartContext);
 
   const handleEmptyCart = () => {
@@ -12,16 +14,18 @@ export const CartContainer = () => {
 
   return (
     <div className="cart-container">
-    {cart.length > 0
-      ? <>
-        <div className="cart__products-container">
-          {cart.map(product => {
-            return <CartProduct product={product} key={product._id} />
-          })}
-        </div>
-        <button onClick={handleEmptyCart} className="primary-button">Vaciar carrito</button>
-      </>
-      : <p>El carrito está vacío</p>}
+    {!user
+      ? <p>Iniciá sesión para poder acceder al carrito</p>
+      : cart.length > 0
+        ? <>
+          <div className="cart__products-container">
+            {cart.map(product => {
+              return <CartProduct product={product} key={product._id} />
+            })}
+          </div>
+          <button onClick={handleEmptyCart} className="primary-button">Vaciar carrito</button>
+        </>
+        : <p>El carrito está vacío</p>}
     </div>
   )
 }
