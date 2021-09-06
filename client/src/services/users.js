3cb288d1b4e5097c5ba3c73be3bc9f9243a4c3ce
Lucas Piputto <lucaspiputto@gmail.com>
2021-09-06 14:46:37 -0300

    Add function to handle product likes

--- client/src/services/users.js
+++ client/src/services/users.js
@@ -3,10 +3,15 @@ const axios = require('axios');
 const baseUrl = '/api/users';
 
 const updateUserBalance = (username, amountToLoad) => {
-  const request = axios.put(`${baseUrl}/${username}`, amountToLoad);
+  const request = axios.put(`${baseUrl}/${username}/balance`, amountToLoad);
   return request.then(res => res.data);
 }
 
-const exportableFunctions = { updateUserBalance };
+const handleProductLike = (username, likedProduct) => {
+  const request = axios.put(`${baseUrl}/${username}/likes`, likedProduct);
+  return request.then(res => res.data);
+}
+
+const exportableFunctions = { updateUserBalance, handleProductLike };
 
 export default exportableFunctions;
