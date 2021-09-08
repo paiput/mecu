d49a114ae51928433a054c94a2d60f986e1b5592
Lucas Piputto <lucaspiputto@gmail.com>
2021-09-07 22:09:21 -0300

    Create function to handle product purchase

--- client/src/services/users.js
+++ client/src/services/users.js
@@ -12,6 +12,23 @@ const handleProductLike = (username, likedProduct) => {
   return request.then(res => res.data);
 }
 
-const exportableFunctions = { updateUserBalance, handleProductLike };
+const handleProductPurchase = (user, product, amountToBuy) => {
+  if (product.price > user.balance) {
+    const requests = {
+      updatedUser: Promise.reject(new Error('failedUser')),
+      updatedProduct: Promise.reject(new Error('failedProduct'))
+    }
+    return requests;
+  }
+
+  const requests = {
+    updatedUser: axios.put(`${baseUrl}/${user.username}/balance`, {amountToLoad: `-${product.price * amountToBuy}`}).then(res => res.data),
+    updatedProduct: axios.put(`/api/products/${product._id}`, {amountToBuy}).then(res => res.data),
+    updatedProductOwner: axios.put(`${baseUrl}/${product.user.username}/balance`, {amountToLoad: `${product.price * amountToBuy}`})
+  }
+  return requests;
+}
+
+const exportableFunctions = { updateUserBalance, handleProductLike, handleProductPurchase };
 
 export default exportableFunctions;
